3a2a82711821ac23bd9b4432b5a0bc0092605651
fassst2 <fasst1@proton.me>
2024-12-17 19:30:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6356732743:AAGDfDYofKORoqhv8hGfvBQSgGnkGYn9pe4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("uzzukiller16")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("933709537")
+
+
+
+
+
+
+
+  
